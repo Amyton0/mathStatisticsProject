eddad61e20c23f89f3ea5b4d7ce1ef57834491cb
Amyton0 <khlopina.sd@gmail.com>
2024-04-19 23:44:25 +0500

    Готовая страница

--- frontend/checking.js
+++ frontend/checking.js
@@ -5,7 +5,9 @@ let homeworks = [
         isSecondTry: true,
         group: 'ФТ-204',
         file: 'hws/HomeworkSemen.pdf',
-        number: 1
+        number: 1,
+        comment: null,
+        points: 0
     }, 
     {
         name: 'Бабинцев Дмитрий',
@@ -13,21 +15,28 @@ let homeworks = [
         isSecondTry: false,
         group: 'ФТ-204',
         file: 'hws/HomeworkDmitry.pdf',
-        number: 2
+        number: 2,
+        comment: null,
+        points: 0
     }
 ]
 
 let filteredHomeworks = homeworks.slice(0);
 
-let chooseNumber = document.getElementById('Number');
-let chooseTry = document.getElementById('SecondTry');
-let chooseGroup = document.getElementById('Group');
+let chooseNumber = document.getElementsByClassName('number')[0];
+let chooseTry = document.getElementsByClassName('secondTry')[0];
+let chooseGroup = document.getElementsByClassName('group')[0];
+
+let send = document.getElementsByClassName('send')[0];
 
 chooseNumber.oninput = onNumberChange;
 
 let isSecondTry = null;
 let currentGroup = 'Любая';
 let currentNumber = '';
+let currentHomework = -1;
+
+send.addEventListener('click', sendMessage)
 
 function onTryChange(event) {
     const selectedValue = event.target.value;
@@ -65,7 +74,7 @@ function filterHWs() {
 
     let table = document.createElement("table");
     
-    let container = document.getElementById("tableContainer");
+    let container = document.getElementsByClassName("tableContainer")[0];
 
     container.removeChild(container.querySelector('table'))
 
@@ -74,27 +83,39 @@ function filterHWs() {
         let row = table.insertRow(-1);
 
         let cell1 = row.insertCell(0);
-        cell1.innerHTML = element.name;
+        cell1.innerText = element.name;
+
+        cell1.addEventListener('click', () => {
+            document.getElementsByClassName("solution")[0].src = element.file;
+            currentHomework = filteredHomeworks.indexOf(element);
+        })
 
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.id = i;
+        checkbox.disabled = true;
         if (element.isChecked) checkbox.checked = true;
-
-        checkbox.addEventListener('click', (event) => {
-            filteredHomeworks[event.target.id].isChecked = !filteredHomeworks[event.target.id].isChecked;
-        })
         
         let label = document.createElement("label");
         label.htmlFor = i;
         label.appendChild(document.createTextNode("Проверено"));
 
         var cell2 = row.insertCell(1);
-        cell2.appendChild(checkbox);
+        
         cell2.appendChild(label);
+        cell2.appendChild(checkbox);
     }
 
     container.appendChild(table);
+}
 
-    console.log(filteredHomeworks)
+function sendMessage() {
+    let points = document.getElementsByClassName("points")[0].value;
+    let message = document.getElementsByClassName("comment")[0].value;
+
+    homeworks[currentHomework].comment = message;
+    homeworks[currentHomework].points = Number(points);
+    homeworks[currentHomework].isChecked = true;
+
+    console.log(homeworks[currentHomework])
 }
