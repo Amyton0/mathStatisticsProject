a80cfde0e3eb8f4cb04bac8c382a1f06fa29e1a7
Amyton0 <khlopina.sd@gmail.com>
2024-05-09 19:21:07 +0500

    Пустые методы для клиента

---
+++ frontend/client.js
@@ -0,0 +1,43 @@
+const API_URL = 'http://localhost:5105/api'
+
+async function getHomeworkJsonAsync(relativeUrl) {
+    
+}
+
+async function postHomeworkJsonAsync(relativeUrl) {
+    
+}
+
+async function postScoreJsonAsync(relativeUrl) {
+    
+}
+
+async function getStudentJsonAsync(relativeUrl) {
+    
+}
+
+async function postStudentJsonAsync(relativeUrl) {
+    
+}
+
+async function getTableJsonAsync(relativeUrl) {
+    
+}
+
+async function logHomeworkJsonAsync(relativeUrl) {
+    
+}
+
+async function logScoreAsync(relativeUrl) {
+    
+}
+
+async function logStudentAsync(relativeUrl) {
+    
+}
+
+async function logTableAsync(relativeUrl) {
+    
+}
+
+
