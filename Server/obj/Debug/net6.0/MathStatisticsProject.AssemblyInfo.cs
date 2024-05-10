b99db8128ded2ada44a826dac797ecc5f09a5e1d
VirteN <jacksppir@gmail.com>
2024-05-10 19:05:11 +0500

    123

--- Server/obj/Debug/net6.0/MathStatisticsProject.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/MathStatisticsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathStatisticsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b239f4d1c2e7d2e440f9557f7258b43342b2379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathStatisticsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathStatisticsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
