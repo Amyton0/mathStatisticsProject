f37bb6fff805cd5c3360a3d09249f11e3f7fe5c8
Amyton0 <khlopina.sd@gmail.com>
2024-05-10 17:32:21 +0500

    Функции get и post для домашек

--- frontend/client.js
+++ frontend/client.js
@@ -1,14 +1,31 @@
 const API_URL = 'http://localhost:5105/api'
 
 async function getHomeworkJsonAsync(relativeUrl) {
-    
+    try {
+        const response = await fetch(`${API_URL}${relativeUrl}`);
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(`Bad response status: ${response.status}`)
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-async function postHomeworkJsonAsync(relativeUrl) {
+async function postHomeworkJsonAsync(homework) {
+    var data = new FormData();
+    data.append( "json", JSON.stringify( homework ) );
     
+    fetch("/echo/json/",
+    {
+        method: "POST",
+        body: data
+    })
+    .then(function(res){ return res.json(); })
+    .then(function(data){ alert( JSON.stringify( data ) ) })
 }
 
-async function postScoreJsonAsync(relativeUrl) {
+async function postScoreJsonAsync(score) {
     
 }
 
@@ -16,27 +33,11 @@ async function getStudentJsonAsync(relativeUrl) {
     
 }
 
-async function postStudentJsonAsync(relativeUrl) {
+async function postStudentJsonAsync(student) {
     
 }
 
 async function getTableJsonAsync(relativeUrl) {
-    
-}
-
-async function logHomeworkJsonAsync(relativeUrl) {
-    
-}
-
-async function logScoreAsync(relativeUrl) {
-    
-}
-
-async function logStudentAsync(relativeUrl) {
-    
-}
-
-async function logTableAsync(relativeUrl) {
     
 }
 
