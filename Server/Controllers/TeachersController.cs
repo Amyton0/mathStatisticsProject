ca1070eff9f41bd27df9a00cfbd006271d079f92
VirteN <jacksppir@gmail.com>
2024-05-15 16:19:23 +0500

    235

---
+++ Server/Controllers/TeachersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using MathStatisticsProject.Data;
+using MathStatisticsProject.GetModels;
+using MathStatisticsProject.Models;
+using MathStatisticsProject.PostModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MathStatisticsProject.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TeachersController : ControllerBase
+{
+    private readonly Context _context;
+    private readonly IMapper _mapper;
+
+    public TeachersController(Context context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetTeacher>> GetTeacher(Guid id)
+    {
+        var teacher = await _context.Teachers.FindAsync(id);
+
+        if (teacher == null)
+        {
+            return NotFound();
+        }
+
+        var getTeacher = _mapper.Map<GetTeacher>(teacher);
+
+        return getTeacher;
+    }
+
+    //CR: нам это не надо пока. Можно оставить, только не забудь, что не надо реализовывать
+    [HttpPost]
+    public async Task<ActionResult<PostStudent>> PostStudent([FromBody] PostStudent student)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Server/PostModels/PostTeacher.cs
@@ -0,0 +1,9 @@
+﻿namespace MathStatisticsProject.PostModels;
+
+public class PostTeacher
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string ThirdName { get; set; }
+}
